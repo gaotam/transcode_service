7ccbe9f335fa516beb350b71005ce859c5b2cdeb
mynamebvh <mynamebvh@gmail.com>
2024-02-26 22:54:58 +0700

    update db live

--- db/live.go
+++ db/live.go
@@ -13,17 +13,11 @@ type VideoByLiveKeyResult struct {
 func GetLiveByLiveKey(liveKey string) (result VideoByLiveKeyResult, err error) {
 	var id, status string
 	var isRecord bool
-	var app string
+
 	err = Connect.QueryRow(context.Background(), "SELECT id, status, \"isRecord\" FROM livestreams WHERE \"liveKey\" = $1", liveKey).Scan(&id, &status, &isRecord)
 	if err != nil {
 		return VideoByLiveKeyResult{}, err
 	}
-
-	if !isRecord {
-		app = "nr_live"
-	} else {
-		app = "live"
-	}
-
+	app := "live"
 	return VideoByLiveKeyResult{Id: id, Status: status, App: app}, nil
 }
